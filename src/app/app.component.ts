43a35e4165b4b9f4948914f1568847d97bd15035
Michael Rogachevsky <mrogach@Michaels-MBP.domain>
2017-05-29 16:29:07 -0500

    updates app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'main-app',
+  selector: 'app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'], 
+  moduleId: module.id
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Hello World!';
 }
