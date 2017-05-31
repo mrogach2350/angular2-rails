1a126e98636bfc8ca86c0dd6571642316736a455
Michael Rogachevsky <m.rogach@gmail.com>
2017-05-30 20:38:10 -0500

    removes moduleId from app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,6 @@ import { Component } from '@angular/core';
   selector: 'app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'], 
-  moduleId: module.id
 })
 export class AppComponent {
   title = 'Hello World!';
