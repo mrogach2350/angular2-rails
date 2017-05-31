948bd77661df9523c20a8f0b3bc9bcfb45d93457
Michael Rogachevsky <m.rogach@gmail.com>
2017-05-30 20:39:06 -0500

    abstracts out homepage template to its own html doc

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -2,6 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'homepage',
-    template: '<h1>Home</h1>'
+    templateUrl: './homepage.component.html'
 })
 export class HomepageComponent {}
